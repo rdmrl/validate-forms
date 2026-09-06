acbd45f6b3fb8d528b63586067849a59d177287e
Ravi Damarla <ravi.damarla@gmail.com>
2026-09-06 21:01:04 +0100

    chore: updated config to fix warnings about __dirname

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,18 @@
 /// <reference types="vitest" />
 import { resolve } from 'node:path'
-import { defineConfig } from 'vite'
-import dts from 'unplugin-dts/vite'
-import camelCase from 'camelcase'
-import packageJson from './package.json'
+import { defineConfig } from 'vite';
+import dts from 'unplugin-dts/vite';
+import camelCase from 'camelcase';
+import packageJson from './package.json' with { type: 'json' };
+import { dirname } from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const packageName = packageJson.name.split('/').pop() || packageJson.name
 
 export default defineConfig({
+  base: "./",
   build: {
     lib: {
       entry: resolve(__dirname, 'src/index.ts'),
@@ -20,4 +25,10 @@ export default defineConfig({
     dts({ rollupTypes: true }),
   ],
   test: {},
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, "src"),
+      "@@": resolve(__dirname),
+    },
+  }
 })
